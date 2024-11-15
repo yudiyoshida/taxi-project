4d04a03909ea498e15b5c423caa2618dfeadbd7f
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-11-15 00:23:07 -0300

    tests: testes de integração no request ride

--- src/modules/ride/use-cases/request-ride/request-ride.controller.spec.ts
+++ src/modules/ride/use-cases/request-ride/request-ride.controller.spec.ts
@@ -1,3 +1,129 @@
+import { Test } from '@nestjs/testing';
+import { PrismaModule } from 'src/infra/database/prisma/prisma.module';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { SignupUseCase } from 'src/modules/account/use-cases/signup/signup.service';
+import { Errors } from 'src/shared/errors/error-message';
+import { RideStatus } from '../../domain/entities/ride.entity';
+import { RideModule } from '../../ride.module';
+import { GetRideByIdUseCase } from '../get-ride-by-id/get-ride-by-id.service';
+import { RequestRideInputDto } from './dtos/request-ride.dto';
+import { RequestRideController } from './request-ride.controller';
+
 describe('RequestRideController', () => {
-  it.todo('implement tests');
+  let sut: RequestRideController;
+  let prisma: PrismaService;
+  let signup: SignupUseCase;
+  let getRideById: GetRideByIdUseCase;
+
+  beforeEach(async() => {
+    const module = await Test.createTestingModule({
+      imports: [
+        RideModule,
+        PrismaModule,
+      ],
+    }).compile();
+
+    sut = module.get<RequestRideController>(RequestRideController);
+    prisma = module.get<PrismaService>(PrismaService);
+    signup = module.get<SignupUseCase>(SignupUseCase);
+    getRideById = module.get<GetRideByIdUseCase>(GetRideByIdUseCase);
+
+    await prisma.account.deleteMany();
+    await prisma.ride.deleteMany();
+  });
+
+  it('should throw an error if provided id does not belong to a passenger', async() => {
+    // Arrange
+    const account = await signup.execute({
+      name: 'Jhon Doe',
+      cpf: '12345678909',
+      email: 'jhondoe@email.com',
+      password: 'any_password',
+      isDriver: true,
+      isPassenger: false,
+      carPlate: 'ABC1234',
+    });
+
+    // Act & Assert
+    return sut.handle({
+      passengerId: account.id,
+      fromLat: 1,
+      fromLng: 2,
+      toLat: 3,
+      toLng: 4,
+    })
+      .catch(error => {
+        expect(error.message).toBe(Errors.ACCOUNT_NOT_PASSENGER_TYPE);
+      });
+  });
+
+  it('should throw an error when the passenger already has an active ride', async() => {
+    // Arrange
+    const account = await signup.execute({
+      name: 'Jhon Doe',
+      cpf: '12345678909',
+      email: 'jhondoe@email.com',
+      password: 'any_password',
+      isDriver: false,
+      isPassenger: true,
+      carPlate: null,
+    });
+
+    const data: RequestRideInputDto = {
+      passengerId: account.id,
+      fromLat: 1,
+      fromLng: 2,
+      toLat: 3,
+      toLng: 4,
+    };
+
+    // Act
+    await sut.handle(data);
+
+    // Assert
+    return sut.handle(data)
+      .catch(error => {
+        expect(error.message).toBe(Errors.PASSENGER_ALREADY_HAS_ACTIVE_RIDE);
+      });
+  });
+
+  it('should create a new ride', async() => {
+    // Arrange
+    const account = await signup.execute({
+      name: 'Jhon Doe',
+      cpf: '12345678909',
+      email: 'jhondoe@email.com',
+      password: 'any_password',
+      isDriver: false,
+      isPassenger: true,
+      carPlate: null,
+    });
+
+    const data: RequestRideInputDto = {
+      passengerId: account.id,
+      fromLat: 1,
+      fromLng: 2,
+      toLat: 3,
+      toLng: 4,
+    };
+
+    // Act
+    const ride = await sut.handle(data);
+    expect(ride).toHaveProperty('id');
+
+    // Assert
+    const result = await getRideById.execute(ride.id);
+    expect(result).toEqual({
+      id: ride.id,
+      passengerId: account.id,
+      driverId: null,
+      date: expect.any(Date),
+      fare: null,
+      status: RideStatus.requested,
+      fromLat: data.fromLat,
+      fromLng: data.fromLng,
+      toLat: data.toLat,
+      toLng: data.toLng,
+    });
+  });
 });
--- todo.md
+++ todo.md
@@ -1,10 +1,7 @@
 # TODO
-- testes unitários na account entity.
-- testes unitários na account factory (método load).
 - testes unitários na ride entity.
 - testes unitários na ride factory (método load).
 
 - transformar ridestatus em vo?
-- testes nos inputDtos.
 
 - prisma multiple env files para testes de integração e e2e.
